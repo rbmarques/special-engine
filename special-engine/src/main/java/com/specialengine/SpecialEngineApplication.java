0ef3629a3e3d8eee6c5b6302b87ddc7ec3aabab2
Raphael Barros Marques <rbarrosmarques@gmail.com>
2019-12-10 16:34:23 -0300

    Create Project

---
+++ special-engine/src/main/java/com/specialengine/SpecialEngineApplication.java
@@ -0,0 +1,13 @@
+package com.specialengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialEngineApplication.class, args);
+	}
+
+}
---
+++ special-engine/src/test/java/com/specialengine/SpecialEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.specialengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
